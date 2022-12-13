fdc3592b69847f896ce00847a80493a58483077e
LuongVanVu <vuluong1337@gmail.com>
2022-12-13 23:40:06 +0700

    fix bai8.c file

--- bai8.c
+++ bai8.c
@@ -10,12 +10,18 @@ int timmax(int x, int y)
         return y;
 }
 
+int tinhtong(int x, int y)
+{
+    return x + y;
+}
+
 int main(int argc, char const *argv[])
 {
     int a = 5, b = 20;
 
-    int max = timmax(a,b);
+    int max = timmax(a, b);
     printf("So lon nhat trong 2 so la: %d", max);
-    
+    printf("\nTong cua 2 so la: %d", tinhtong(a,b));
+
     return 0;
 }
