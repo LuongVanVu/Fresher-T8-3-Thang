989538576316d095c25e668a0d47a6e080f8c1b5
LuongVanVu <vuluong1337@gmail.com>
2022-12-13 23:37:08 +0700

    tim max

--- bai8.c
+++ bai8.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+int timmax(int x, int y)
+{
+    if (x >= y)
+
+        return x;
+
+    else
+        return y;
+}
+
 int main(int argc, char const *argv[])
 {
-    printf("Hello World!!");
-    printf("Xin chao viet nam!!");
+    int a = 5, b = 20;
+
+    int max = timmax(a,b);
+    printf("So lon nhat trong 2 so la: %d", max);
+    
     return 0;
 }
